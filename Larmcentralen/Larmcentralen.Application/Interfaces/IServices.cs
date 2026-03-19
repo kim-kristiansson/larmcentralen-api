0756843922f00f1f2af03629c87257aac7d61043
kim-kristiansson <154594963+kim-kristiansson@users.noreply.github.com>
2026-03-20 00:51:20 +0100

    feat: add service interfaces

---
+++ Larmcentralen/Larmcentralen.Application/Interfaces/IServices.cs
@@ -0,0 +1,39 @@
+﻿using Larmcentralen.Application.DTOs;
+
+namespace Larmcentralen.Application.Interfaces;
+
+public interface IAreaService
+{
+    Task<List<AreaDto>> GetAllAsync();
+    Task<AreaDto?> GetByIdAsync(int id);
+    Task<AreaDto> CreateAsync(CreateAreaDto dto);
+    Task<bool> UpdateAsync(int id, UpdateAreaDto dto);
+    Task<bool> DeleteAsync(int id);
+}
+
+public interface IEquipmentService
+{
+    Task<List<EquipmentDto>> GetAllAsync(int? areaId);
+    Task<EquipmentDto?> GetByIdAsync(int id);
+    Task<EquipmentDto> CreateAsync(CreateEquipmentDto dto);
+    Task<bool> UpdateAsync(int id, UpdateEquipmentDto dto);
+    Task<bool> DeleteAsync(int id);
+}
+
+public interface IAlarmService
+{
+    Task<List<AlarmListDto>> SearchAsync(string? search, int? equipmentId, string? severity);
+    Task<AlarmDto?> GetByIdAsync(int id);
+    Task<AlarmDto> CreateAsync(CreateAlarmDto dto);
+    Task<bool> UpdateAsync(int id, UpdateAlarmDto dto);
+    Task<bool> DeleteAsync(int id);
+}
+
+public interface ISolutionService
+{
+    Task<List<SolutionDto>> GetByAlarmIdAsync(int alarmId);
+    Task<SolutionDto?> GetByIdAsync(int id);
+    Task<SolutionDto> CreateAsync(CreateSolutionDto dto);
+    Task<bool> UpdateAsync(int id, UpdateSolutionDto dto);
+    Task<bool> DeleteAsync(int id);
+}
