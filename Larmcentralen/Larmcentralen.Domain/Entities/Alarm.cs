eb54f74c0797acb8b071e6ef9d2a978cc9bf3902
kim-kristiansson <154594963+kim-kristiansson@users.noreply.github.com>
2026-03-20 00:41:48 +0100

    feat: add dbcontext

--- Larmcentralen/Larmcentralen.Domain/Entities/Alarm.cs
+++ Larmcentralen/Larmcentralen.Domain/Entities/Alarm.cs
@@ -7,10 +7,11 @@ public class Alarm
     public string? AlarmCode { get; set; }
     public string Severity { get; set; } = "Låg";
     public string? Description { get; set; }
-    public string? Solution { get; set; }
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     public DateTime? UpdatedAt { get; set; }
 
     public int EquipmentId { get; set; }
     public Equipment Equipment { get; set; } = null!;
+
+    public List<Solution> Solutions { get; set; } = [];
 }
---
+++ Larmcentralen/Larmcentralen.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,57 @@
+﻿using Larmcentralen.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Larmcentralen.Infrastructure.Data;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    public DbSet<Area> Areas => Set<Area>();
+    public DbSet<Equipment> Equipment => Set<Equipment>();
+    public DbSet<Alarm> Alarms => Set<Alarm>();
+    public DbSet<Solution> Solutions => Set<Solution>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Area>(e =>
+        {
+            e.HasIndex(a => a.Title).IsUnique();
+            e.Property(a => a.Title).HasMaxLength(100);
+        });
+
+        modelBuilder.Entity<Equipment>(e =>
+        {
+            e.Property(eq => eq.Title).HasMaxLength(200);
+            e.Property(eq => eq.Category).HasMaxLength(50);
+
+            e.HasOne(eq => eq.Area)
+                .WithMany(a => a.Equipment)
+                .HasForeignKey(eq => eq.AreaId)
+                .OnDelete(DeleteBehavior.Restrict);
+        });
+
+        modelBuilder.Entity<Alarm>(e =>
+        {
+            e.Property(a => a.Title).HasMaxLength(300);
+            e.Property(a => a.AlarmCode).HasMaxLength(50);
+            e.Property(a => a.Severity).HasMaxLength(20);
+
+            e.HasOne(a => a.Equipment)
+                .WithMany(eq => eq.Alarms)
+                .HasForeignKey(a => a.EquipmentId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            e.HasIndex(a => new { a.AlarmCode, a.EquipmentId }).IsUnique();
+        });
+
+        modelBuilder.Entity<Solution>(e =>
+        {
+            e.Property(s => s.Title).HasMaxLength(300);
+            e.Property(s => s.EstimatedTime).HasMaxLength(50);
+
+            e.HasOne(s => s.Alarm)
+                .WithMany(a => a.Solutions)
+                .HasForeignKey(s => s.AlarmId)
+                .OnDelete(DeleteBehavior.Restrict);
+        });
+    }
+}
