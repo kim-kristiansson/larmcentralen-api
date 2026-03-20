baec51cbe8ef04e1a13a67a3e2fe75d87e4bfa97
kim-kristiansson <154594963+kim-kristiansson@users.noreply.github.com>
2026-03-20 01:45:04 +0100

    feat: add Swagger UI

--- Larmcentralen/Larmcentralen.Api/Program.cs
+++ Larmcentralen/Larmcentralen.Api/Program.cs
@@ -8,17 +8,14 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
 builder.Services.AddControllers()
     .AddJsonOptions(o =>
     {
         o.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
     });
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 // Database
 builder.Services.AddDbContext<AppDbContext>(options =>
@@ -38,16 +35,13 @@ builder.Services.AddScoped<ISolutionService, SolutionService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
